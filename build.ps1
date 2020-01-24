2a4f44140878fc1e66891f511142c2860210fd33
Lee-Roy Ashworth <leeroya@gmail.com>
2020-01-24 23:16:01 +0200

    feat: added build scripts

--- build.ps1
+++ build.ps1
@@ -1,2 +1,39 @@
-tns build android --release --key-store-path <path-to-your-keystore> --key-store-password <your-key-store-password> --key-store-alias <your-alias-name> --key-store-alias-password <your-alias-password>
-keytool -genkey -v -keystore <my-release-key>.keystore -alias <alias_name> -keyalg RSA -keysize 2048 -validity 10000
+
+param(
+    $Type = 'Dev',
+    $Alias = 'alias_name',
+    $KeyStore = '.\build\destiny-key.keystore'
+)
+
+$Key = $Env:CHURCH;
+if('Release' -eq $Type){
+    $Path = "$PSScriptRoot\App_Resources\Android\src\main\AndroidManifest.xml";
+    $APK = "$PSScriptRoot\platforms\android\app\build\outputs\apk\release\app-release.apk";
+
+    [xml]$xmlDoc = New-Object system.Xml.XmlDocument;
+    [xml]$xmlDoc = Get-Content $Path;
+
+    $currentVersion = $xmlDoc.manifest.versionName;
+    $versionCode = $xmlDoc.manifest.versionCode -as [int];
+    $currentVersionArray = $currentVersion.Split('.');
+    $Patch = $currentVersionArray | Select-Object -Last 1;
+    $IntPatch = $Patch -as [int];
+    $IntPatch++;
+    $versionCode++;
+
+    $FinalBuildNumber = "$($currentVersionArray[0]).$($currentVersionArray[1]).$($IntPatch)";
+
+    $xmlDoc.manifest.versionName = $FinalBuildNumber;
+    $xmlDoc.manifest.versionCode = "$($versionCode)";
+    $xmlDoc.Save($Path);
+
+    tns build android --release --key-store-path $KeyStore --key-store-password $Key --key-store-alias $Alias --key-store-alias-password $Key
+    try {
+        Copy-Item "$APK" -Destination "$PSScriptRoot\dist\destiny.release.$FinalBuildNumber.apk"
+    }
+    catch {
+     Write-Error "The artifact is missing" -ErrorAction:Stop;
+    }
+}
+
+#keytool -genkey -v -keystore <my-release-key>.keystore -alias <alias_name> -keyalg RSA -keysize 2048 -validity 10000
