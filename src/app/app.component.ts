0a9f29587c7123da3130a9f2ed2eb435834cfa2e
Lee-Roy Ashworth <info@leeroya.com>
2020-01-29 22:46:05 +0200

    feat: wired the reciving of push messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+const firebase = require("nativescript-plugin-firebase");
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
@@ -11,6 +11,31 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        firebase.init({
+            // Optionally pass in properties for database, authentication and cloud messaging,
+            // see their respective docs.
+            showNotificationsWhenInForeground: true,
+            showNotifications: true,
+            onPushTokenReceivedCallback: function(token) {
+                console.log("Firebase push token: " + token);
+              },
+              onMessageReceivedCallback: (message: any) => {
+                console.log(`Title: ${message.title}`);
+                console.log(`Body: ${message.body}`);
+                // if your server passed a custom property called 'foo', then do this:
+                console.log(`Value of 'foo': ${message.data.foo}`);
+              }
+          }).then(
+            () => {
+              console.log("firebase.init done");
+            },
+            error => {
+              console.log(`firebase.init error: ${error}`);
+            }
+          );
+          firebase.getCurrentPushToken().then((token: string) => {
+            // may be null if not known yet
+            console.log(`Current push token: ${token}`);
+          });
     }
 }
